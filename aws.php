7ebc45be784a8e82e34b505e64d82dd1182c79e0
WBMclear <matsuitomoharu587@gmail.com>
2024-01-15 12:43:32 +0900

    phpファイル

---
+++ aws.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+$su=10;
+$ui=10;
+$sum=$su*$ui;
+echo $sum;
+?>
+</body>
+</html>
